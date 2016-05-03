982f9424b82addfd3ee5f3fb029399a03846a9c0
John Wiseman <jjwiseman@gmail.com>
2016-05-03 14:32:21 -0700

    Test mod for charting.

--- test/com/lemondronor/pid_test.clj
+++ test/com/lemondronor/pid_test.clj
@@ -4,20 +4,21 @@
 
 (deftest pid-test
   (testing "basic functionality"
-    (let [goal 0]
-      (loop [pid (pid/pid
-                  {:kp 2
-                   :ki 1/30
-                   :kd 1/2
-                   :set-point goal
-                   :bounds [-180 180 -1 1]
-                   :period-ms 10})
-             current 30
-             n 0]
-        (when (< n 100)
-          (let [[pid v] (pid/pid pid (* n 10) current)
-                new-current (+ current v)]
-            (println n new-current)
-            (is (< (Math/abs (- goal new-current))
-                   (Math/abs (- goal current))))
-            (recur pid new-current (inc n))))))))
+    (loop [pid (pid/pid
+                {:kp 2
+                 :ki 1/30
+                 :kd 1/2
+                 :set-point 0
+                 :bounds [-180 180 -1 1]
+                 :period-ms 100})
+           current 30
+           goal 0
+           n 0]
+      (when (< n 300)
+        (let [pid (assoc pid :set-point goal)
+              [pid v] (pid/pid pid (* n 100) current)
+              new-current (+ current v)]
+          (println n new-current v)
+;;          (is (< (Math/abs (- goal new-current))
+;;                 (Math/abs (- goal current))))
+          (recur pid new-current (if (< n 200) 0 30) (inc n)))))))
