04397b36fcca57fa1811e2dcbf3fe89eed136753
John Wiseman <jjwiseman@gmail.com>
2016-05-02 23:42:19 -0700

    .cljc-ified.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
+  :source-paths ["src/cljc"]
   :dependencies [[org.clojure/clojure "1.8.0"]])
