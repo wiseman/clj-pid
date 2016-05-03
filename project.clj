a6036184156d8ea31c40588683282651a6f5bd8e
John Wiseman <jjwiseman@gmail.com>
2016-05-02 21:37:06 -0700

    Added clojars as the deploy repo.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://github.com/wiseman/clj-pid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.8.0"]])
