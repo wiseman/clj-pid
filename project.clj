5d890a46d1791a85cb736a43b8bb5d2ae2f512ba
John Wiseman <jjwiseman@gmail.com>
2016-05-06 12:10:39 -0700

    Version 4.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemondronor/pid "4.0.4"
+(defproject com.lemondronor/pid "4.0.5-SNAPSHOT"
   :description "PID controller in Clojure. Code for writing PID control loops."
   :url "http://github.com/wiseman/clj-pid"
   :license {:name "Eclipse Public License"
