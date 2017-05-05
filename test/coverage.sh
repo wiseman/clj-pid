53a2981838d881fbf113489ccadb69d078234af3
John Wiseman <jjwiseman@gmail.com>
2017-05-04 21:35:46 -0700

    Added debugging to coverage.sh.

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,3 +1,4 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.9' lein2 cloverage -o cov --coveralls
+cat cov/coveralls.json
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
