1edc8e0d191784b6fef2fd0468c91081d87b9c52
John Wiseman <jjwiseman@gmail.com>
2017-05-04 21:44:52 -0700

    More debugging.

--- test/coverage.sh
+++ test/coverage.sh
@@ -1,4 +1,6 @@
 COVERALLS_URL='https://coveralls.io/api/v1/jobs'
 CLOVERAGE_VERSION='1.0.9' lein2 cloverage -o cov --coveralls
 cat cov/coveralls.json
+echo "TRAVIS=$TRAVIS"
+echo "TRAVIS_JOB_ID=$TRAVIS_JOB_ID"
 curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
